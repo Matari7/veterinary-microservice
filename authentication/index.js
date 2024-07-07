79d79615e12d6089a17588a9e31e3635df67bfac
Matari7 <arimcf10@hotmail.es>
2024-07-07 13:58:40 -0500

    Update JWT

--- authentication/index.js
+++ authentication/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(bodyParser.json());
+
+const SECRET_KEY = 'mysecretkey';
 
 const users = [
   { username: 'admin', password: 'password' }
@@ -12,7 +15,8 @@ app.post('/login', (req, res) => {
   const { username, password } = req.body;
   const user = users.find(u => u.username === username && u.password === password);
   if (user) {
-    res.status(200).send('Login successful');
+    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
+    res.status(200).json({ token });
   } else {
     res.status(401).send('Incorrect credentials');
   }
--- list-pets/index.js
+++ list-pets/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const Redis = require('ioredis');
+const jwt = require('jsonwebtoken');
 
 const redis = new Redis({ host: 'redis' }); // Connect to Redis
 const app = express();
+const SECRET_KEY = 'mysecretkey';
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 
 app.get('/pets', async (req, res) => {
   const pets = await redis.lrange('pets', 0, -1); // Get all pets from Redis
   const result = pets.map(pet => JSON.parse(pet));
-  res.status(200).json(result);
+  res.status(200).json({ user: req.user, mascotas: result });
 });
 
 app.listen(3001, () => {
